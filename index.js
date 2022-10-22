018df10ea8924dfc1f6187568b1e28a20c8b1c86
lehoang8d@gmail.com <lehoang8d@gmail.com>
2022-10-22 20:07:18 +0700

    fix cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const socket = require("socket.io");
 const app = express();
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use("/api/auth", userRoutes)
 app.use("/api/message", messageRoutes)
