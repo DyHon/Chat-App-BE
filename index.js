6c82702279b5f9faff56cc1b7573ae05cf000885
lehoang8d@gmail.com <lehoang8d@gmail.com>
2022-10-26 19:34:51 +0700

    fix

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const socket = require("socket.io");
 const app = express();
 require('dotenv').config();
 
-app.use(cors({
-  origin: 'https://zyhon.vercel.app'
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", userRoutes)
 app.use("/api/message", messageRoutes)
