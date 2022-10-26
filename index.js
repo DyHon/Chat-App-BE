6bdfdb64f527cd494b921f71ebbaaa1ec9577f7f
lehoang8d@gmail.com <lehoang8d@gmail.com>
2022-10-26 17:23:34 +0700

    fix bug 3

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 
+const http = require('http');
+
 const socket = require("socket.io");
 const app = express();
 require('dotenv').config();
 
-app.use(cors({origin: '*'}));
+app.use(cors({origin: "*"}));
 app.use(express.json());
 app.use("/api/auth", userRoutes)
 app.use("/api/message", messageRoutes)
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next()
+});
 
 async function connect() {
   try {
@@ -25,24 +33,23 @@ async function connect() {
 
 connect();
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server Started on PORT ${process.env.PORT}`);
-});
-
-
-const io = socket(server, {
+const httpServer = http.createServer();
+const io = new socket.Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "https://zyhonserver.vercel.app",
     credentials: true,
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
   }
-});
+})
 
 global.onlineUsers = new Map();
 
 io.on('connection', (socket) => {
   global.chatSocket = socket;
+  console.log("connecting...")
   socket.on('add-user', (userId) => {
+    console.log("add user")
     onlineUsers.set(userId, socket.id);
     console.log("connect socket successfully");
   });
